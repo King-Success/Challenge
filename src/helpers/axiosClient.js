9fb13199eb80559dc2c4631f039213d78edf44c0
Kingsley Arinze <arinze.kingsley2012@gmail.com>
2019-10-16 19:25:06 +0100

    chore(env & http client): setup app's env variables and http client

---
+++ src/helpers/axiosClient.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const {
+  REACT_APP_API_KEY: ApiKey,
+  REACT_APP_API_BASE: baseURL,
+  NODE_ENV: environ
+} = process.env;
+const headers = { Authorization: `Bearer ${ApiKey}` };
+
+const axiosClient =
+  environ === "test" ? axios : axios.create({ baseURL, headers });
+export default axiosClient;
